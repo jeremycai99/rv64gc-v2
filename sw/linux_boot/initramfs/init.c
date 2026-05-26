ab235d190da859644536543838a8c944aa61b52f
jeremycai99 <cyjcyj1999@gmail.com>
2026-05-25 23:07:40 -0700

    stage3: make initramfs boot marker console-visible

--- sw/linux_boot/initramfs/init.c
+++ sw/linux_boot/initramfs/init.c
@@ -1,10 +1,23 @@
 #define _GNU_SOURCE
 
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/sysmacros.h>
 #include <unistd.h>
 
 int main(void) {
     static const char msg[] = "\nBOOT OK\n";
-    (void)write(STDOUT_FILENO, msg, sizeof(msg) - 1);
+    int console_fd;
+
+    (void)mkdir("/dev", 0755);
+    (void)mknod("/dev/console", S_IFCHR | 0600, makedev(5, 1));
+
+    console_fd = open("/dev/console", O_WRONLY | O_NOCTTY);
+    if (console_fd >= 0) {
+        (void)write(console_fd, msg, sizeof(msg) - 1);
+    } else {
+        (void)write(STDOUT_FILENO, msg, sizeof(msg) - 1);
+    }
 
     for (;;) {
         __asm__ volatile("" ::: "memory");
